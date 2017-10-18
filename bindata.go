7b3a784153e6cbdd067a1ed5492bc790da5a4b17
Cheikh Seck <Cheikh@pc164.home>
2017-10-18 22:38:08 +0000

    updates

--- bindata.go
+++ bindata.go
@@ -204,6 +204,7 @@ type bintree struct {
 	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
+
 var _bintree = &bintree{nil, map[string]*bintree{
 	"web": &bintree{nil, map[string]*bintree{
 		"your-404-page.tmpl": &bintree{webYour404PageTmpl, map[string]*bintree{}},
@@ -257,4 +258,3 @@ func _filePath(dir, name string) string {
 	cannonicalName := strings.Replace(name, "\\", "/", -1)
 	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
 }
-
